1ecd5ddc599f6c0de6c82563683b0d8f726ed624
kitekii <tim720719@163.com>
2021-03-24 19:01:31 +0800

    add a tool to create calibration target

---
+++ src/tool/CalibrationTargetGen/CalibrationTargetGen.cpp
@@ -0,0 +1,48 @@
+/**
+ * Calibration Target Generator
+ * 
+ * 
+ * Easy tool for camera calibration without printing a extra image board.
+ * Optifined with global error from float. You can edit parameters commented below.
+ * 
+ * Kitekii 2021 copyright.
+ */
+#include <iostream>
+#include <opencv2\opencv.hpp>
+using namespace std;
+using namespace cv;
+
+// 显示器点距 (mm)
+// screen model: 276E9FJAB
+const float PIX_DISTANCE = 0.2331f;
+
+constexpr int castToPixel(const float worldPos, const float pixDistance = PIX_DISTANCE) noexcept
+{
+    return static_cast<int>(worldPos / pixDistance);
+}
+
+void main()
+{
+
+    // =================================================
+    
+    const int n_cols = 4;                           // 角点行数
+    const int n_rows = 6;                           // 角点列数
+    const float block_length = 20.f;                // 世界坐标边距 (mm)
+    const int border_pix = 20;                      // 生成标定图的边距(pix)
+
+    // =================================================
+    
+    const float col_length = block_length * n_cols;
+    const float row_length = block_length * n_rows;
+    const int col_pix = castToPixel(col_length);
+    const int row_pix = castToPixel(row_length);
+    Mat map = Mat(border_pix * 2 + row_pix, border_pix * 2 + col_pix, CV_8UC3, Scalar::all(0));
+    for (int i = 0; i < row_pix; i++)
+        for (int j = 0; j < col_pix; j++)
+            if (int(1.0f * i / row_pix * n_rows) % 2 == 0 && int(1.0f * j / col_pix * n_cols) % 2 != 0 || int(1.0f * i / row_pix * n_rows) % 2 != 0 && int(1.0f * j / col_pix * n_cols) % 2 == 0)
+                map.at<Vec3b>(border_pix + i, border_pix + j)[0] = map.at<Vec3b>(border_pix + i, border_pix + j)[1] = map.at<Vec3b>(border_pix + i, border_pix + j)[2] = 255;
+    imshow("map", map);
+    waitKey(0);
+    return;
+}
