7955eb1c428b474613bbfaa882a1e534c57dd2a9
kitekii <tim720719@163.com>
2021-03-31 09:53:01 +0800

    + camera calibration util

---
+++ include/IKMvs/CameraCalibration/Calibration.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <Ikit/STL/Singleton.h>
+#include <fstream>
+#include <string>
+namespace KTKR::MVS
+{
+    class CalibrationManager : public KTKR::Singleton<CalibrationManager>
+    {
+    private:
+        cv::Size board_size;
+        cv::Mat camera_matrix;
+        cv::Mat dist_coeffs;
+
+    public:
+        CalibrationManager()
+        {
+            camera_matrix = cv::Mat(3, 3, CV_32FC1, cv::Scalar::all(0));
+            dist_coeffs = cv::Mat(1, 5, CV_32FC1, cv::Scalar::all(0));
+        }
+
+        bool SaveCalibrationParameters(std::string filepath) noexcept
+        {
+            std::ofstream fout(filepath);
+            fout << "intrinsic matrix:" << std::endl;
+            fout << camera_matrix << std::endl
+                 << std::endl
+                 << "distortion parameters:" << std::endl
+                 << dist_coeffs << std::endl;
+            fout.close();
+        }
+        bool ReadCalibrationParameters(std::string filepath) noexcept
+        {
+            std::ifstream fin(filepath);
+            if (fin)
+            {
+                char read[100];
+                double getdata;
+                fin >> read >> read;
+                fin.seekg(3, std::ios::cur);
+                for (int j = 0; j < 3; j++)
+                    for (int i = 0; i < 3; i++)
+                    {
+                        fin >> getdata;
+                        camera_matrix.at<float>(j, i) = static_cast<float>(getdata);
+                        fin >> read;
+                    }
+                fin >> read >> read;
+                fin.seekg(3, std::ios::cur);
+                for (int i = 0; i < 5; i++)
+                {
+                    fin >> getdata;
+                    dist_coeffs.at<float>(i) = static_cast<float>(getdata);
+                    fin >> read;
+                }
+                fin.close();
+                return true;
+            }
+            return false;
+        }
+        void SetBoardSize(cv::Size boardSize = cv::Size(7, 5)) { board_size = std::move(boardSize); }
+        cv::Mat GetCameraMatrix() noexcept { return camera_matrix; }
+    };
+
+} // namespace KTKR::MVS
