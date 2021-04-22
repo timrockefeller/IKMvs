f4902eca0fc2fc4db8a380149dc44ccae989f5a7
kitekii <tim720719@163.com>
2021-04-22 16:28:22 +0800

    + multi view test

---
+++ src/test/06_MultiImageMatch/06_MultiImageMatch.cpp
@@ -0,0 +1,31 @@
+// ================================================= //
+//                                                   //
+//                  solvePnPRansac                   //
+//                                                   //
+// ================================================= //
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+#include <IKMvs/CameraCalibration/Calibration.h>
+using namespace std;
+using namespace cv;
+using namespace KTKR::MVS;
+
+const string images [6]= {
+    "../asset/sanae/1.JPG",
+    "../asset/sanae/2.JPG",
+    "../asset/sanae/3.JPG",
+    "../asset/sanae/4.JPG",
+    "../asset/sanae/5.JPG",
+    "../asset/sanae/6.JPG"
+};
+
+
+int main()
+{
+    
+    
+
+    return 0;
+}
