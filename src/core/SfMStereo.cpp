cf27c34f5828fc8f66468044fc1d3e032578520d
kitekii <tim720719@163.com>
2021-04-26 12:50:35 +0800

    + triangulation

--- src/core/SfMStereo.cpp
+++ src/core/SfMStereo.cpp
@@ -41,10 +41,12 @@ ErrorCode SfMStereo::findCameraMatricesFromMatch(const Intrinsics &intrinsics,
     }
 
     Features alignedL, alignedR;
-    GetAlignedPointsFromMatch(featureL, featureR, matches, alignedL, alignedR);
+    GetAlignedPointsFromMatch(featureL, featureR, matches,
+                              alignedL, alignedR);
 
     Mat mask;
-    auto essentialMat = cv::findEssentialMat(alignedL.points, alignedR.points, intrinsics.K, RANSAC, 0.999, 1.0, mask);
+    auto essentialMat = cv::findEssentialMat(alignedL.points, alignedR.points, intrinsics.K,
+                                             RANSAC, 0.999, 1.0, mask);
 
     Mat R, T;
     cv::recoverPose(essentialMat, alignedL.points, alignedR.points, intrinsics.K, R, T);
@@ -59,6 +61,79 @@ ErrorCode SfMStereo::findCameraMatricesFromMatch(const Intrinsics &intrinsics,
     for (size_t i = 0; i < mask.rows; i++)
         if (mask.at<uchar>(i))
             prunedMatches.push_back(matches[i]);
+
+    return OK;
+}
+
+ErrorCode SfMStereo::triangulateViews(const Intrinsics &intrinsics,
+                                      const ImagePair imagePair,
+                                      const Matching &matches,
+                                      const Features &featureL,
+                                      const Features &featureR,
+                                      cv::Matx34f &Pleft,
+                                      cv::Matx34f &Pright,
+                                      PointCloud &pointCloud)
+{
+    if (intrinsics.K.empty())
+    {
+        cerr << "Intrinsics matrix(K) must be initialized." << endl;
+        return ErrorCode::ERR_RUNTIME_ABORT;
+    }
+
+    vector<int> backRefL;
+    vector<int> backRefR;
+    Features alignedL;
+    Features alignedR;
+    GetAlignedPointsFromMatch(featureL, featureR, matches,
+                              alignedL, alignedR,
+                              backRefL, backRefR);
+
+    Mat normPtsL;
+    Mat normPtsR;
+    undistortPoints(alignedL.points, normPtsL, intrinsics.K, intrinsics.distortion);
+    undistortPoints(alignedR.points, normPtsR, intrinsics.K, intrinsics.distortion);
+
+    // =================================================
+
+    Mat pts_4d;
+    cv::triangulatePoints(Pleft, Pright, normPtsL, normPtsR, pts_4d);
+    Mat pts_3d;
+    cv::convertPointsFromHomogeneous(pts_4d.t(), pts_3d);
+
+    Mat rvecLeft;
+    Rodrigues(Pleft.get_minor<3, 3>(0, 0), rvecLeft);
+    Mat tvecLeft(Pleft.get_minor<3, 1>(0, 3).t());
+    Points2f projectedOnLeft(alignedL.points.size());
+    projectPoints(pts_3d, rvecLeft, tvecLeft, intrinsics.K, Mat(), projectedOnLeft);
+
+    Mat rvecRight;
+    Rodrigues(Pright.get_minor<3, 3>(0, 0), rvecRight);
+    Mat tvecRight(Pright.get_minor<3, 1>(0, 3).t());
+    Points2f projectedOnRight(alignedR.points.size());
+    projectPoints(pts_3d, rvecRight, tvecRight, intrinsics.K, Mat(), projectedOnRight);
+
+    // =================================================
+
+    for (size_t i = 0; i < pts_3d.rows; i++)
+    {
+        //check if point reprojection error is small enough
+        if (norm(projectedOnLeft[i] - alignedL.points[i]) > MIN_REPROJECTION_ERROR ||
+            norm(projectedOnRight[i] - alignedR.points[i]) > MIN_REPROJECTION_ERROR)
+        {
+            continue;
+        }
+
+        Point3DInMap p;
+        p.p = Point3f(pts_3d.at<float>(i, 0),
+                      pts_3d.at<float>(i, 1),
+                      pts_3d.at<float>(i, 2));
+
+        //use back reference to point to original features in images
+        p.originatingViews[imagePair.left] = backRefL[i];
+        p.originatingViews[imagePair.right] = backRefR[i];
+
+        pointCloud.push_back(p);
+    }
 
     return OK;
 }
