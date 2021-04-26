037a431abb788f791cd87dcad37536aef0e5b7e7
kitekii <tim720719@163.com>
2021-04-26 15:16:19 +0800

    add dep: ceres

--- src/core/SfMBundleAdjustment.cpp
+++ src/core/SfMBundleAdjustment.cpp
@@ -1,16 +1,31 @@
 #include <IKMvs/SfM/SfMBundleAdjustment.h>
 #include <IKMvs/Config.h>
 #include <Ikit/STL/ILog.h>
-
+#include <ceres/ceres.h>
+#include <ceres/rotation.h>
 using namespace std;
 using namespace cv;
 using namespace KTKR;
 using namespace KTKR::MVS;
 
-void adjustBundle(
+namespace KTKR::Private
+{
+    void initLogging()
+    {
+        google::InitGoogleLogging("SFM");
+    }
+    std::once_flag initLoggingFlag;
+}
+
+void SfMBundleAdjustment::adjustBundle(
     PointCloud &pointCloud,
     std::vector<Pose> &cameraPoses,
     Intrinsics &intrinsics,
     const std::vector<Features> &image2dFeatures)
 {
+    std::call_once(Private::initLoggingFlag, Private::initLogging);
+
+    ceres::Problem problem;
+
+
 }
